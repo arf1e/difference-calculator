d18f644283eb71420a40e7bde737ff93ccee4316
arf1e <anotheruselessbox@gmail.com>
2021-11-03 16:16:06 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,11 @@ import { Command } from 'commander';
 
 const program = new Command();
 
-program.version('0.0.1').description('Compares two configuration files and shows a difference.');
+program
+  .arguments('<filepath1> <filepath2>')
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
